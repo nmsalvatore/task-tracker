aef7fe89ce817101a6f925e679b1749472fc0730
Nick Salvatore <nmsalvatore@gmail.com>
2025-12-06 18:56:37 -0800

    refactor: use concatenation instead of fmt.Sprintf

--- tasks.go
+++ tasks.go
@@ -1,10 +1,8 @@
 package main
 
-import "fmt"
-
 func AddTask(task string, status string) string {
 	if status == "" {
 		status = "todo"
 	}
-	return fmt.Sprintf("%s: %s", status, task)
+	return status + ": " + task
 }
