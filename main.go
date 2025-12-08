8b015f994939db25b5ec58d3588c2d922e38ac45
Nick Salvatore <nmsalvatore@gmail.com>
2025-12-07 17:15:58 -0800

    feat: move user flow to run(), execute run() in main()

--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
+	err := run()
+	if err != nil {
+		log.Fatalf("run failed: %v", err)
+	}
+}
+
+func run() error {
 	tasks := Tasks{}
 
 	err := tasks.Add("", "make dinner", "go on a walk")
 	if err != nil {
-		log.Fatalf("failed to add tasks: %v", err)
+		return fmt.Errorf("failed to add tasks: %v", err)
 	}
 
-	tasks.Add("done", "drink coffee")
+	err = tasks.Add("done", "drink coffee")
 	if err != nil {
-		log.Fatalf("failed to add tasks: %v", err)
+		return fmt.Errorf("failed to add tasks: %v", err)
 	}
 
 	tasks.List(os.Stdout)
+	return nil
 }
